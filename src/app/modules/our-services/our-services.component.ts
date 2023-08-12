f1893a0049546cbd678bad2669a32226e8c43fb9
tagore9930 <tagore9930@gmail.com>
2023-08-12 20:43:21 +0530

    Added Dynamic data

--- src/app/modules/our-services/our-services.component.ts
+++ src/app/modules/our-services/our-services.component.ts
@@ -6,15 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-services.component.scss']
 })
 export class ServicesComponent {
-  servicesList = new Array(10);
   value = '';
   previousFilter: string = "col-6";
   previousFilterType: string = "filter-double";
+  servicesList = [
+    {
+      img: '/assets/services-images/behavior-classes.jpg',
+      name: 'Dog behavior Training',
+      amount: '$30.00'
+    },
+    {
+      img: '/assets/services-images/dog-day-activity.jpg',
+      name: 'Dog Day Activity',
+      amount: '$10.00'
+    },
+    {
+      img: '/assets/services-images/doggy-camp.jpg',
+      name: 'Doggy Day Camp',
+      amount: '$5.00'
+    },
+    {
+      img: '/assets/services-images/health-package.jpg',
+      name: 'Dog Health Package',
+      amount: '$50.00'
+    },
+    {
+      img: '/assets/services-images/pet-sitting.jpg',
+      name: 'Pet Sitting',
+      amount: '$20.00'
+    },
+    {
+      img: '/assets/services-images/training-webinar.jpg',
+      name: 'Dog Training Webinar',
+      amount: '$60.00'
+    }
+  ];
 
 
   constructor() { }
 
-  filter(filter: string, filterType: string) {
+  columsFilter(filter: string, filterType: string) {
     let cards = document.querySelectorAll(".cards");
     document.querySelector("." + this.previousFilterType)?.classList.remove("active")
     document.querySelector("." + filterType)?.classList.add("active")
@@ -22,5 +53,11 @@ export class ServicesComponent {
     this.previousFilter = filter;
     this.previousFilterType = filterType;
   }
+
+  sreachFilter() {
+    
+  }
+
+
 
 }
